b93cd0c45c5d72e3e23f3a37e2e34a799fb3d005
kiran.mehetre <mehetrekiran2@gmail.com>
2017-12-27 11:56:20 +0530

    add

--- crm.module.ts
+++ crm.module.ts
@@ -5,16 +5,28 @@ import { Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { CRMComponent } from './crm.component';
+import { ProductComponent } from './product/product.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const crmRouting: Routes = [
   {
       path: '',
       component: CRMComponent
-  }
+  },
+  {
+    path: 'Product',
+    component: ProductComponent
+},
+{
+  path: 'AboutUs',
+  component: AboutUsComponent
+}
 ]
 @NgModule({
   declarations: [
     CRMComponent,
+    ProductComponent,
+    AboutUsComponent,
   ],
   imports: [
     ReactiveFormsModule,
